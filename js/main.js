fa88dc36eb93268bc65b9e8c3a777fbd38b3451e
erceth <erceth@gmail.com>
2015-05-22 00:06:27 -0600

    added logo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-    $(".hamburger").on("click", function() {
-        $(".mobile-navigation").slideToggle(function() {
-
-        });
+    $(".hamburger").on("click", function(e) {
+        $(".mobile-navigation").slideToggle();
+        e.stopPropagation();
+    });
+    $("body").on("click", function() {
+    	$(".mobile-navigation").slideUp();
     });
     setTimeout(function() {
     	$("body").removeClass("preload");	
     }, 2000);
+
+    $(".logo").one("mouseover click", {}, function() {
+    	$(this).append("<img class='flame flame1' src='../img/flame.gif''>");
+    	$(this).append("<img class='flame flame2' src='../img/flame.gif''>");
+    	$(this).append("<img class='flame flame3' src='../img/flame.gif''>");
+    	$(this).append("<img class='flame flame4' src='../img/flame.gif''>");
+    	$(this).append("<img class='flame flame5' src='../img/flame.gif''>");
+    	$(this).append("<audio autoplay><source src='/sound/missle_launch.ogg' type='audio/ogg'><source src='missle_launch.mp3' type='audio/mpeg'><source src='missle_launch.wav' type='audio/wav'></audio>");
+
+    	$(this).animate({
+    		position: "relative",
+    		top: "-100px",
+    		left: "63px"
+    	}, 1000);
+    });
     
 });
 
